8938e6fa9cbd319f94086ef02d16b08396f1176e
@chandan195 <chandanmariya98@gmail.com>
2022-03-23 22:29:32 +0530

    some changes in page not found

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,15 +1,15 @@
 import React from "react";
 import pnf from "../../images/pnf.jpg"
 import "../PageNotFound/PageNotFound.css"
-import Headers from "../Header/Headers";
+// import Headers from "../Header/Headers";
 
 const PageNotFound = () => {
   return (
     
     <div> 
-      <div>
+      {/* <div>
         <Headers/>
-        </div> 
+        </div>  */}
       
       <div className="pnf-image">
         <img src={pnf} alt="pnf" />
